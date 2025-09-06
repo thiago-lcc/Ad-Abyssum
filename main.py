a156553fd449feea0b966f20cce8967e56257621
Thiago <thiagoluizcastro2006@gmail.com>
2025-09-06 00:55:06 -0300

    Add rudimentary light mechanics

--- main.py
+++ main.py
@@ -21,12 +21,35 @@ speed = 200 #later put this inside each entity's class
 
 
 def darkness_setup() -> None:
-   
-  darkness.fill((0, 0, 0, 220)) #creates the darkness
+  
+  # where the light "spawns" from
+  px = player.x + player.width/2 
+  py = player.y + player.height/2 - 50
 
-  pygame.draw.circle(darkness, (0, 0, 0, 0), (player.x + player.width/2, player.y + player.height/2), 120) #creates the "light"
 
-  win.get_screen().blit(darkness, (0, 0))#draws both
+
+  angle = 0.0  # direction the flashlight is pointing. Ex:. 0 = right, pi/2 = up, etc.
+  beam_len = 320
+  beam_half_angle = math.radians(30)  # angle from the light coming out of the flashlight
+
+
+  # every corner of the light beam
+  p1 = (px, py)
+  p2 = (int(px + math.cos(angle - beam_half_angle) * beam_len),
+      int(py + math.sin(angle - beam_half_angle) * beam_len))
+  p3 = (int(px + math.cos(angle + beam_half_angle) * beam_len),
+      int(py + math.sin(angle + beam_half_angle) * beam_len))
+
+
+
+  darkness.fill((0, 0, 0, 220)) # creates the darkness
+
+
+  pygame.draw.circle(darkness, (0, 0, 0, 0), (px, py), 120) # creates light circle
+  pygame.draw.polygon(darkness, (0, 0, 0, 0), [p1, p2, p3]) # creates light beam
+
+
+  win.get_screen().blit(darkness, (0, 0))# draws all
 
 
 
